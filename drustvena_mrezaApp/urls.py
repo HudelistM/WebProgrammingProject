c61817c3f1d473bf26d0518529f418697b2e04c8
Marin Hudelist <marin.hudelist@gmail.com>
2023-09-03 21:44:22 +0200

    Added login and register

--- drustvena_mrezaApp/urls.py
+++ drustvena_mrezaApp/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from .views import pocetna, lista_profila, profil,lista_objava
+from django.contrib.auth import views as auth_views
+from . import views as app_views
+from . import views
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 app_name = "drustvena_mrezaApp"
 
@@ -8,4 +14,9 @@ urlpatterns = [
     path("lista_profila/", lista_profila, name="lista_profila"),
     path("lista_objava/", lista_objava, name="lista_objava"),
     path("profil/<int:pk>", profil, name="profil"),
+    path("register/", app_views.register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="drustvena_mreza/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="drustvena_mreza/logout.html"), name="logout"),
+    path('homepage/', views.homepage, name='homepage'),
+    path('moj_profil/', views.moj_profil, name='moj_profil'),
 ]
--- drustvena_mrezaApp/views.py
+++ drustvena_mrezaApp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from .models import Profil,Objava
+from .forms import RegisterForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 User=get_user_model()
 # Create your views here.
@@ -17,3 +20,27 @@ def lista_objava(request):
 def profil(request, pk):
     profil = Profil.objects.get(pk=pk)
     return render(request, "drustvena_mreza/profil.html", {"profil": profil})
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}!")
+            return redirect("drustvena_mrezaApp:login")
+    else:
+        form = RegisterForm()
+    return render(request, "drustvena_mreza/register.html", {"form": form})
+def homepage(request):
+    user = request.user
+    profil = Profil.objects.get(user=user)
+    following_users = profil.follows.all()
+    posts = Objava.objects.filter(user__in=following_users).order_by('-datum_objave')
+    return render(request, 'drustvena_mreza/homepage.html', {'posts': posts})
+
+def moj_profil(request):
+    user = request.user
+    profil = Profil.objects.get(user=user)
+    following_users = profil.follows.all()
+    posts = Objava.objects.filter(user=user).order_by('-datum_objave')
+    return render(request, 'drustvena_mreza/moj_profil.html', {'following_users': following_users, 'posts': posts})
